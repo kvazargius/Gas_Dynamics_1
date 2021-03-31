161e8f5dea0110f8a77dd53d7ae2682c0b32cd32
kvazargius <novitskiybronislav@yandex.ru>
2021-03-31 13:25:02 +0300

    Добавлен расчёт сопла на расчётном режиме

--- DZ/DZ_1/Laval nozzle.py
+++ DZ/DZ_1/Laval nozzle.py
@@ -1,3 +1,198 @@
 import numpy as np
+import matplotlib.pyplot as plt
+from scipy.optimize import fsolve
+
+
+'''
+Программа по проверка ДЗ № 1
+'''
+
+
+G = float(input('Расход, кг/с = '))
+T_t = float(input('Температура торможения, К = '))
+p_t = float(input('Давление торможения, Па = '))
+alfa_g = float(input('угол полураскрытия диффузора (alfa/2 ), град = '))
+R = float(input('Газовая постоянная, Дж / (кг*К) = '))
+k = float(input('Показатель адиабаты = '))
+h = float(input('Высота полёта, км = ')) * 1000
+
 
 pi = np.pi
+
+alfa = alfa_g * np.pi / 180
+
+pi_g = [0,0,0,0,0,0]
+T = [0,0,0,0,0,0]
+T1 = [0,0,0,0,0,0]
+ro = [0,0,0,0,0,0]
+S = [0,0,0,0,0,0]
+r = [0,0,0,0,0,0]
+d = [0,0,0,0,0,0]
+V = [0,0,0,0,0,0]
+a = [0,0,0,0,0,0]
+d = [0,0,0,0,0,0]
+p = [0,0,0,0,0,0]
+lam = [0,0,1,0,0,0]
+M = [0,0,0,0,0,0]
+gdf_g = [0,0,0,0,0,0]
+
+
+def p_alt(h):
+    return 101325 * (1 - ((0.0065 / 288.15) * h)) ** (9.80665 / (0.0065 * 287.053))
+
+
+def ro_gdf(lam):
+    return (1 - (k - 1) / (k + 1) * lam ** 2) ** (1 / (k - 1))
+
+
+def pi_gdf(lam):
+    return (1 - (k - 1) / (k + 1) * lam ** 2) ** (k / (k - 1))
+
+
+def t_gdf(lam):
+    return (1 - (k - 1) / (k + 1) * lam ** 2)
+
+
+def g_gdf(lam):
+    return ((k + 1) / 2) ** (1/ (k - 1)) * (1 - (k - 1)/(k + 1) * lam ** 2) ** (1/(k - 1)) * lam
+
+
+def v_max(Temp):
+    return np.sqrt(2 * k / (k - 1) * R * Temp)
+
+
+def akr(Temp):
+    return np.sqrt(2 * k / (k + 1) * R * Temp)
+
+
+def lam_from_pi(pi_g):
+    return np.sqrt((1-pi_g ** ((k - 1) / k)) * (k+1)/(k-1))
+
+
+def diam(S):
+    return np.sqrt(4 * S / np.pi)
+
+
+def soplo_lav():
+    ro_t = p_t / (R * T_t)
+
+    #  Параметры в критическом сечении
+    T[2] = T_t * t_gdf(1)
+    p[2] = p_t * pi_gdf(1)
+    ro[2] = ro_t * ro_gdf(1)
+    V[2] = akr(T_t)
+    a_kr = akr(T_t)
+    #  Площадь критического сечения
+    S[2] = G / (ro[2] * a_kr)
+    d[2] = diam(S[2])
+
+    #  Параметры при срезе (сечение 5)
+    pi_g[5] = p_0 / p_t
+    lam[5] = lam_from_pi(pi_g[5])
+    V[5] = lam[5] * V[2]
+    T[5] = T_t * t_gdf(lam[5])
+    ro[5] = ro_t * ro_gdf(lam[5])
+    a[5] = np.sqrt(k * R * T[5])
+    gdf_g[5] = g_gdf(lam[5])
+
+    #  Площадь среза
+    S[5] = S[2] / g_gdf(lam[5])
+    d[5] = diam(S[5])
+
+    #  Геометрия сопла
+    l2 = (d[5] - d[2]) / (2 * np.tan(alfa))  # сверхзвуковая часть
+    l1 = 2 * d[2]
+    l = l1 + l2
+    d[0] = d[2] * 2
+    d[1] = d[2] * 1.5
+    d[3] = d[2] + l2 / 3 * 2 * np.tan(alfa)
+    d[4] = d[2] + 2 * l2 / 3 * 2 * np.tan(alfa)
+
+    S[0] = np.pi / 4 * d[0] ** 2
+    S[1] = np.pi / 4 * d[1] ** 2
+    S[3] = np.pi / 4 * d[3] ** 2
+    S[4] = np.pi / 4 * d[4] ** 2
+
+    gdf_g[0] = S[2] / S[0]
+    gdf_g[1] = S[2] / S[1]
+    gdf_g[3] = S[2] / S[3]
+    gdf_g[4] = S[2] / S[4]
+
+    def f_g_gdf_0(x):
+        return g_gdf(x) - gdf_g[0]
+
+    lam[0] = float(fsolve(f_g_gdf_0, 0))
+    print(lam[0])
+
+    def f_g_gdf_1(x):
+        return g_gdf(x) - gdf_g[1]
+
+    lam[1] = float(fsolve(f_g_gdf_1, 0))
+    print(lam[1])
+
+    def f_g_gdf_3(x):
+        return g_gdf(x) - gdf_g[3]
+
+    lam[3] = float(fsolve(f_g_gdf_3, 1.9))
+    print(lam[3])
+
+    def f_g_gdf_4(x):
+        return g_gdf(x) - gdf_g[4]
+
+    lam[4] = float(fsolve(f_g_gdf_4, 1.9))
+    print(lam[4])
+
+
+    # Оставшиеся параметры
+    for i in range(6):
+        T[i] = T_t * t_gdf(lam[i])
+        a[i] = np.sqrt(k * R * T[i])
+        V[i] = lam[i] * a_kr
+        p[i] = p_t * pi_gdf(lam[i])
+
+    for i in range(6):
+        lam[i] = float('{:.3f}'.format(lam[i]))
+        p[i] = float('{:.1f}'.format(p[i]))
+        V[i] = float('{:.1f}'.format(V[i]))
+        T[i] = float('{:.1f}'.format(T[i]))
+        a[i] = float('{:.1f}'.format(a[i]))
+        r[i] = d[i] / 2 * 1000
+        M[i] = V[i] / a[i]
+        M[i] = float('{:.2f}'.format(M[i]))
+        r[i] = float('{:.2f}'.format(r[i]))
+        d[i] = float('{:.4f}'.format(d[i]))
+
+    print('a кр = ', a_kr)
+    print('Ламбда = ', lam)
+    print('p =  ', p, 'Па')
+    print('a =  ', a, 'м/с')
+    print('V =  ', V, 'м/с')
+    print('М =  ', M, '')
+    print('T =  ', T, 'K')
+    print('r = ', r, 'мм')
+    print('d = ', d, 'м')
+
+    print('_____Длина сопла_______')
+    print('Длина дозвуковой части = ', l1, 'Длина сверхзвуковой части = ', l2)
+    print('Общая длина = ', l1 + l2)
+
+    #  Графики
+    x = [1, 2, 3, 4, 5, 6]
+
+    plt.grid()  # включение отображение сетки
+    plt.xlabel("сечения, номера точек")  # ось абсцисс
+    plt.ylabel("м/с")  # ось ординат
+
+    plt.vlines(3, V[0], V[5], color='r')  # Вертикальные линии
+
+    plt.plot(x, a, 'r--', label='скорость звука')
+    plt.plot(x, V, 'g', label='скорость V')
+    plt.legend()
+
+    plt.show()
+
+    return p, lam
+
+
+p_0 = p_alt(h)
+pp, lamb = soplo_lav()
